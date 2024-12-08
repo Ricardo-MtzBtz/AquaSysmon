0755e83efe66ad443e19725a4da6d0055bdd14f1
Ricardo-MtzBtz <160088817+Ricardo-MtzBtz@users.noreply.github.com>
2024-12-08 16:39:43 -0600

    Create run_dht22.sh

---
+++ run_dht22.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /path/to/your/venv/bin/activate #Cambiar respecto a la ruta de almacenamiento de tu entorno virtual anteriormente creado
+python /path/to/your_script.py #Cambiar respecto a la ruta de almacenamiento de tu script de lectura del sensor dht ubicado  en el entorno virutal
+deactivate
