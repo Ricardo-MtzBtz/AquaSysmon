181c5b692f590c01cc110d0837b4275338f9230d
Ricardo-MtzBtz <160088817+Ricardo-MtzBtz@users.noreply.github.com>
2024-12-08 16:40:58 -0600

    Update read_temp.py

--- read_temp.py
+++ read_temp.py
@@ -1,12 +1,12 @@
+# read_temp.py
+
 from w1thermsensor import W1ThermSensor
 
 # Inicializa el sensor
 sensor = W1ThermSensor()
 
-# Lee la temperatura en grados Celsius y Fahrenheit
+# Lee la temperatura en grados Celsius
 temperature_celsius = sensor.get_temperature()
-temperature_fahrenheit = sensor.get_temperature(W1ThermSensor.DEGREES_F)
 
-# Imprime las temperaturas
-print(f"{temperature_celsius:.2f} °C")
-print(f"{temperature_fahrenheit:.2f} °F")
+# Imprime la temperatura
+print(f"{temperature_celsius:.2f}")
